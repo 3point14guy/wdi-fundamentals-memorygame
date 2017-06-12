de1e89d5136df24ec8deb6e1336a0eaf677f0f6f
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-06-11 21:22:28 -0400

    created final steps for game

--- js/main.js
+++ js/main.js
@@ -28,11 +28,12 @@ console.log("You found a match!");
 console.log("Sorry, try again.");
 }
 };
-var flipCard = function(cardId) {
-
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id")
 console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
 console.log(cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute("src", cards[cardId].cardImage);
 checkForMatch();
 
 if (cardsInPlay.length < 2) {
@@ -42,5 +43,14 @@ if (cardsInPlay.length < 2) {
 } else {alert("Sorry, try again.");
 }
 };
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+		
+}
+};
+createBoard();
